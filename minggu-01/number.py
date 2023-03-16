f706327ff22f4b75d89bc67c57991d8dc1b8ce1b
zefnathC <zefnathnendissa@gmail.com>
2023-03-16 08:51:18 +0000

    tes2

--- minggu-01/number.py
+++ minggu-01/number.py
@@ -2,4 +2,25 @@ print(2 + 2)
 print (50 - 5*6)
 print ((50 - 5*6)/4)
 print (8/5)
+print(17/3)
+print(17//3)
+print(17 % 3)
+print(5*3+2)
+print(5**2)
+print(2**7)
+print("=============")
+width = 20
+height = 5 * 9
+print (width * height)
+print("=============")
+print(4 * 3.75 - 1)
+print("=============")
+
+tax = 12.5 /100
+price = 100.50
+print(price * tax)
+
+
+
+
 
